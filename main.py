c383c15b24f92a25f9033a59468bc4ceeb54a368
Victor Burtsev <vicbur@bk.ru>
2025-01-30 14:53:42 +0300

    mask_account_card is added to main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
-print(get_mask_card_number(7000792289606361))
+# print(get_mask_card_number(7000792289606361))
 # 7000792289606361     # входной аргумент
 # 7000 79** **** 6361  # выход функции
 
 
-print(get_mask_account(73654108430135874305))
+# print(get_mask_account(73654108430135874305))
 # 73654108430135874305  # входной аргумент
 # **4305  # выход функции
+
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Maestro 1596837868705199"))
+# Пример для карты
+# Visa Platinum 7000792289606361  # входной аргумент
+# Visa Platinum 7000 79** **** 6361  # выход функции
+
+# Пример для счета
+# Счет 73654108430135874305  # входной аргумент
+# Счет **4305  # выход функции
--- src/widget.py
+++ src/widget.py
@@ -21,20 +21,12 @@ def mask_account_card(input_long_data:str) -> str:
         long_input_masked_low = " ".join(input_data_list)
         long_input_masked = long_input_masked_low.title()
     else:
+        # если был счет, то вызываем функцию маскировки номера счета, на
+        # выходе получаем маску номера счета
         mask_2 = get_mask_account(num_code)
         input_data_list[-1] = mask_2
         long_input_masked_low = " ".join(input_data_list)
         long_input_masked = long_input_masked_low.title()
 
-        print(long_input_masked)
-
     return long_input_masked
 
-mask_account_card("Счет 73654108430135874305")
-# Пример для карты
-# Visa Platinum 7000792289606361  # входной аргумент
-# Visa Platinum 7000 79** **** 6361  # выход функции
-
-# Пример для счета
-# Счет 73654108430135874305  # входной аргумент
-# Счет **4305  # выход функции
