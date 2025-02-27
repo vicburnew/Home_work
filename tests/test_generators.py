3f77a3deff9ead7beb8782b5af9b92a91d99bdf9
Victor Burtsev <vb4oot@gmail.com>
2025-02-27 14:25:06 +0300

    1. in generators.py is added filter_by_currency 2. conftest is updates fix fixtures 3. test_generators.py is added with first positive test

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,5 +2,6 @@ from src.generators import filter_by_currency
 
 
 def test_filter_by_currency(transactions, transactions_USD_1):
-    assert filter_by_currency(transactions, "usd") == transactions_USD_1
+    a = filter_by_currency(transactions, "USD")
+    assert list(next(a)) == transactions_USD_1
 
