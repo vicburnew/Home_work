3e4251d5d9454a7abbdd9e9fd7ae93697bb075d6
Victor Burtsev <vb4oot@gmail.com>
2025-03-04 09:34:16 +0300

    log decorator is refactored to handle exceptions,  two negative tests are added to test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,3 +1,6 @@
+from logging import exception
+
+
 def log(filename=""):
     """Декоратор, который автоматически логирует начало и конец выполнения функции,
     а также ее результаты или возникшие ошибки. Принимает необязательный аргумент
@@ -8,16 +11,24 @@ def log(filename=""):
     Имя функции и результат выполнения при успешной операции.
     Имя функции, тип возникшей ошибки и входные параметры,
     если выполнение функции привело к ошибке."""
+
     def decorator(function):
         def wrapper(*args, **kwargs):
-            result = function(*args, **kwargs)
-            if filename:
-                with open (filename, "w", encoding="utf-8") as file:
-                    file.write(f"{function.__name__} the result is {result}, OK")
-            print(f"{function.__name__} the result is {result}, OK")
+            try:
+                result = function(*args, **kwargs)
+                if filename:
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(f"{function.__name__} the result is {result}, OK")
+                print(f"{function.__name__} the result is {result}, OK")
+            except:
+                if filename:
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(f"{function.__name__} error:{exception.__name__}. Inputs:{args}")
+                print(f"{function.__name__} error:{exception.__name__}. Inputs:{args}")
+                raise Exception
 
             return result
+
         return wrapper
+
     return decorator
-
-
