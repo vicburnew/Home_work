f378cb6ebfadde73da42251f20d4d9e4098e62a1
Victor Burtsev <vb4oot@gmail.com>
2025-03-07 09:14:59 +0300

    function read_json_file is added with core reading, test_utils.py is added with first positive test

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,15 @@
+import json
+
 def read_json_file(path_to_file:str) -> list[dict]:
     """Функция принимает на вход путь до JSON-файла и
     возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден,
     функция возвращает пустой список."""
+    full_path = path_to_file + "operations.json"
+    with open (full_path, "r", encoding="utf-8") as json_file:
+        list_result = json.load(json_file)
 
-    list_result = []
-    pass
     return list_result
+
+print(read_json_file("../data/"))
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,5 +2,6 @@ from src.utils import read_json_file
 
 
 def test_read_json_file_1(json_list_initial):
-    """Положительный тест"""
+    """Положительный тест на чтение существующего файла"""
     assert read_json_file("../data/") == json_list_initial
+
