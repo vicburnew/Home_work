ffc8ac4829d735a1f1f74b30012d8bc88936c9d7
Victor Burtsev <vb4oot@gmail.com>
2025-03-07 14:12:02 +0300

    formatting (black, flake8, isort), HTML tests results

--- src/external_api.py
+++ src/external_api.py
@@ -1,15 +1,16 @@
 import os
 
-from dotenv import load_dotenv
 import requests
+from dotenv import load_dotenv
 
-def convert_currency_to_rub(transaction:dict)->float:
+
+def convert_currency_to_rub(transaction: dict) -> float:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции
-     (amount) в рублях, тип данных — float. Если транзакция была в USD или EUR,
-     происходит обращение к внешнему API для получения текущего курса валют
-     и конвертации суммы операции в рубли.
-     Для конвертации валюты используется Exchange Rates Data API:
-     https://apilayer.com/exchangerates_data-api. """
+    (amount) в рублях, тип данных — float. Если транзакция была в USD или EUR,
+    происходит обращение к внешнему API для получения текущего курса валют
+    и конвертации суммы операции в рубли.
+    Для конвертации валюты используется Exchange Rates Data API:
+    https://apilayer.com/exchangerates_data-api."""
     currency_code = transaction["operationAmount"]["currency"]["code"]
     currency_amount = transaction["operationAmount"]["amount"]
 
@@ -23,13 +24,14 @@ def convert_currency_to_rub(transaction:dict)->float:
     url = "https://api.apilayer.com/exchangerates_data/convert"
     payload = {"amount": currency_amount, "from": currency_code, "to": "RUB"}
     headers = {"apikey": api_key}
+
     response = requests.get(url, headers=headers, params=payload)
     if response.status_code != 200:
-        raise Exception ("проверьте параметры запроса и повторите его")
+        raise Exception("проверьте параметры запроса и повторите его")
+    response.raise_for_status()
 
     result = response.json()
 
-    rub_amount = round(float(result["result"]),2)
+    rub_amount = round(float(result["result"]), 2)
 
     return rub_amount
-
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,38 +1,45 @@
+from unittest.mock import patch
+
 import pytest
 
 from src.external_api import convert_currency_to_rub
-from unittest.mock import patch
-
 
 
 @patch("requests.get")
 def test_convert_currency_to_rub_1(mocked_get, transactions_USD_1):
     """Положительный тест на работу функции по USD с симуляцией ответа от ресурса"""
     mocked_get.return_value.status_code = 200
-    mocked_get.return_value.json.return_value = {'date': '2025-03-07',
-            'info': {'rate': 89.299623, 'timestamp': 1741339623},
-            'query': {'amount': 9824.07, 'from': 'USD', 'to': 'RUB'},
-            'result': 877285.747326,
-            'success': True}
+    mocked_get.return_value.json.return_value = {
+        "date": "2025-03-07",
+        "info": {"rate": 89.299623, "timestamp": 1741339623},
+        "query": {"amount": 9824.07, "from": "USD", "to": "RUB"},
+        "result": 877285.747326,
+        "success": True,
+    }
     result = convert_currency_to_rub(transactions_USD_1)
     assert result == 877285.75
+
 
 @patch("requests.get")
 def test_convert_currency_to_rub_2(mocked_get, transactions_eur_1):
     """Положительный тест на работу функции по EUR с симуляцией ответа от ресурса"""
     mocked_get.return_value.status_code = 200
-    mocked_get.return_value.json.return_value = {'date': '2025-03-07',
-        'info': {'rate': 96.872028, 'timestamp': 1741341183},
-         'query': {'amount': 9724.07, 'from': 'EUR', 'to': 'RUB'},
-        'result': 941990.381314,
-        'success': True}
+    mocked_get.return_value.json.return_value = {
+        "date": "2025-03-07",
+        "info": {"rate": 96.872028, "timestamp": 1741341183},
+        "query": {"amount": 9724.07, "from": "EUR", "to": "RUB"},
+        "result": 941990.381314,
+        "success": True,
+    }
     result = convert_currency_to_rub(transactions_eur_1)
     assert result == 941990.38
 
+
 def test_convert_currency_to_rub_3(transactions_RUB_1):
-    """Положительный тест на работу функции по RUB """
+    """Положительный тест на работу функции по RUB"""
     result = convert_currency_to_rub(transactions_RUB_1)
     assert result == 43318.34
+
 
 @patch("requests.get")
 def test_convert_currency_to_rub_4(mocked_get, transactions_USD_1):
@@ -40,4 +47,3 @@ def test_convert_currency_to_rub_4(mocked_get, transactions_USD_1):
     mocked_get.return_value.status_code = 201
     with pytest.raises(Exception):
         convert_currency_to_rub(transactions_USD_1)
-
