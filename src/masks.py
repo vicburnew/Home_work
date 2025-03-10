7ee6c33fd37ffdf61676252e19e247a1b5e28a7a
Victor Burtsev <vb4oot@gmail.com>
2025-03-10 17:45:19 +0300

    module masks.py is added with logging, test logs are written into masks.log

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,29 @@
+import logging
+
+# Создание объекта логера для записи событий
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("./logs/masks.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты в виде числа
     и возвращает маску номера по правилу XXXX XX** **** XXXX"""
 
     if not isinstance(card_number, int):
+        logger.error("Ошибочный тип данных")
         raise TypeError("Ошибка типа данных")
 
     card_number_str = str(card_number)
 
-    # if len(card_number_str) == 0:
-    #     raise TypeError('Неверный номер карты')
-
     if len(card_number_str) != 16:
+        logger.error("Неверный номер карты")
         raise ValueError("Неверный номер карты")
+
+    logger.info("Начало маскирования номер карты")
 
     group_1 = card_number_str[:4] + " "
     group_2 = card_number_str[4:8]
@@ -19,6 +31,8 @@ def get_mask_card_number(card_number: int) -> str:
     group_4 = card_number_str[12:]
     group_2_new = group_2[:2] + "** "
     card_number_mask = group_1 + group_2_new + group_3 + group_4
+    logger.info("Маскирование номер карты завершено успешно")
+
     return card_number_mask
 
 
@@ -27,15 +41,17 @@ def get_mask_account(user_account: int) -> str:
     возвращает маску номера по правилу **XXXX"""
 
     if not isinstance(user_account, int):
+        logger.error("Ошибочный тип данных")
         raise TypeError("Ошибка типа данных")
 
     account_str = str(user_account)
 
-    # if len(account_str) == 0:
-    #     raise TypeError('Неверный номер карты')
-
     if len(account_str) != 20:
-        raise ValueError("Неверный номер карты")
+        logger.error("Неверная длина номера счета")
+        raise ValueError("Неверный номер счета")
 
+    logger.info("Начало маскирования номера счета")
     account_mask = "**" + account_str[-4:]
+    logger.info("Маскирование номера счета завершено успешно")
+
     return account_mask
